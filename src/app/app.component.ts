587854630396f841f26cc2d3a9ef7efdbb70819d
Kapil Raghuwanshi <Kapil.Raghuwanshi5@gmail.com>
2018-05-31 11:18:18 +0530

    basic search functionalities by name, fetching data from http rest service and basic UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { GithubRestApiService } from '../providers/github-rest-api.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'First Angular - Kapil!!';
+
+  inputName: string = "";
+  userDataSet: any;
+  userDataItems: any = [];
+  userDetails: any = [];
+
+  constructor(private gitRestService: GithubRestApiService) {
+
+  }
+
+  searchByName() {
+    this.gitRestService.getGithubUsersByName(this.inputName)
+      .then(responseData => {
+        this.userDataSet = responseData;
+        console.log(this.userDataSet);
+        this.userDataItems = this.userDataSet.items;
+        console.log(this.userDataItems);
+      }
+}
+
+  getUserFullDetails() {
+    this.gitRestService.getGithubUserDetails(this.inputName)
+      .then(responseData => {
+        this.userDetails = responseData;
+        console.log(this.userDetails);
+      }
+}
+
 }
--- src/providers/github-rest-api.service.ts
+++ src/providers/github-rest-api.service.ts
@@ -1,8 +1,39 @@
 import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class GithubRestApiService {
 
-  constructor() { }
+  public gitUsersData: any;
+
+  constructor(private httpServ: Http) {
+
+  }
+
+  //method to fetch Github users data from REST Api by making http request
+  getGithubUsersByName(searchName) {
+    return new Promise(resolve => {
+      this.httpServ.get("https://api.github.com/search/users?q=" + searchName)
+        .map(resp => resp.json())
+        .subscribe(tempdata => {
+          this.gitUsersData = tempdata;
+          resolve(this.gitUsersData);
+        });
+    });
+  }
+
+
+  //method to fetch Githb users data from REST Api by making http request
+  getGithubUserDetails(userTrueName) {
+    return new Promise(resolve => {
+      this.httpServ.get("https://api.github.com/users/" + userTrueName + "/repos")
+        .map(resp => resp.json())
+        .subscribe(tempdata => {
+          this.gitUsersData = tempdata;
+          resolve(this.gitUsersData);
+        });
+    });
+  }
 
 }
