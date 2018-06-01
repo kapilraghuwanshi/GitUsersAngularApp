9721c98c538f0623a27515258f4d9ddd233d1df7
Kapil Raghuwanshi <Kapil.Raghuwanshi5@gmail.com>
2018-06-01 16:20:21 +0530

    Updated sorting functionalities and detailed table of github repositories

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,8 +13,10 @@ export class AppComponent {
   userDataSet: any;
   userDataItems: any = [];
   userDetails: any = [];
+  selectedSort: string = "NameAlphabeticallyAZ";
 
-  pages: number = 5;
+  filteredItems: any = [];
+  pages: number;
   pageSize: number = 3;
   pageNumber: number = 0;
   currentIndex: number = 1;
@@ -22,43 +24,87 @@ export class AppComponent {
   pageStart: number = 1;
 
   constructor(private gitRestService: GithubRestApiService) {
-    
-  }
-
-  // Search by name method
-  searchByName() {
-    this.gitRestService.getGithubUsersByName(this.inputName)
+    //default call for Shantanu on initialization
+    this.gitRestService.getGithubUsersByName("Shantanu")
       .then(responseData => {
         this.userDataSet = responseData;
         this.totalUserCount = this.userDataSet.total_count;
-        // console.log(this.totalUserCount);
-        //  console.log(this.userDataSet);
         this.userDataItems = this.userDataSet.items;
+        this.filteredItems = this.userDataItems;
         this.initPagination();
-        //  console.log(this.userDataItems);
       });
+    console.log(this.userDataItems);
   }
 
-   // Get full details method
+  // Search by name method - if name empty then default otherwise by name
+  searchByName() {
+    if (this.inputName != "") {
+      this.gitRestService.getGithubUsersByName(this.inputName)
+        .then(responseData => {
+          this.userDataSet = responseData;
+          this.totalUserCount = this.userDataSet.total_count;
+          this.userDataItems = this.userDataSet.items;
+          this.filteredItems = this.userDataItems;
+          this.initPagination();
+        });
+    }
+    else {
+      //default search by shantanu
+      this.gitRestService.getGithubUsersByName("Shantanu")
+        .then(responseData => {
+          this.userDataSet = responseData;
+          this.totalUserCount = this.userDataSet.total_count;
+          this.userDataItems = this.userDataSet.items;
+          this.filteredItems = this.userDataItems;
+          this.initPagination();
+        });
+    }
+
+  }
+
+  // Get full details method
   getUserFullDetails(userTrueName) {
+
     this.gitRestService.getGithubUserDetails(userTrueName)
       .then(responseData => {
         this.userDetails = responseData;
-        //console.log(this.userDetails);
-        // this.userName = this.userDetails[1].owner.login;
-        // console.log(this.userName);
+        console.log(this.userDetails);
       });
   }
 
-  //Pagination method
+  //Sorting method
+  SortingResult() {
+    if (this.selectedSort === "NameAlphabeticallyAZ") {
+      this.userDataItems.sort();
+    }
+    else if (this.selectedSort === "NameReverseAlphabeticallyZA") {
+      this.userDataItems.reverse();
+    }
+    else if (this.selectedSort === "RankUp") {
+      console.log("Inside RankUp");
+      this.userDataItems.sort((a,b)=>{
+        return a.score < b.score;
+      });
+    }
+    else if (this.selectedSort === "RankDown") {
+      console.log("Inside RankDown");
+      this.userDataItems.sort((a, b) => {
+        return a.score > b.score;
+      });
+    }
+
+  }
+
+
+  //Pagination methods
   initPagination() {
     this.currentIndex = 1;
     this.pageStart = 1;
-    this.pages = 5;
+    this.pages = this.filteredItems.length / this.pageSize;
 
-    this.pageNumber = parseInt("" + (this.userDataItems.length / this.pageSize));
+    this.pageNumber = parseInt("" + (this.filteredItems.length / this.pageSize));
 
-    if (this.userDataItems.length % this.pageSize != 0) {
+    if (this.filteredItems.length % this.pageSize != 0) {
       this.pageNumber++;
     }
 
@@ -70,8 +116,17 @@ export class AppComponent {
     console.log("this.pageNumber :  " + this.pageNumber);
   }
 
+  fillArray(): any {
+    var obj = new Array();
+    for (var index = this.pageStart; index < this.pageStart + this.pages; index++) {
+      obj.push(index);
+    }
+    return obj;
+  }
+
   refreshItems() {
-    this.userDataItems = this.userDataItems.slice((this.currentIndex - 1) * this.pageSize, (this.currentIndex) * this.pageSize);
+    this.userDataItems = this.filteredItems.slice((this.currentIndex - 1) * this.pageSize, (this.currentIndex) * this.pageSize);
+    this.pagesIndex = this.fillArray();
   }
 
   prevPage() {
@@ -83,6 +138,7 @@ export class AppComponent {
     }
     this.refreshItems();
   }
+
   nextPage() {
     if (this.currentIndex < this.pageNumber) {
       this.currentIndex++;
